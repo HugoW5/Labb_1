cea32b03c7c0c5266768a659de6204f9f6954d6c
HugoW5 <hugo05.wakt@gmail.com>
2024-10-23 20:55:16 +0200

    Add project files.

---
+++ Labb_1/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Labb_1
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Circle c = new Circle(10);
+			Console.WriteLine(c.ToString());
+		}
+	}
+
+	class Circle
+	{
+		public double Radius { get; set; }
+		public Circle(double radius)
+		{
+			Radius = radius;
+		}
+
+		public double GetArea()
+		{
+			double area = Radius * Radius * Math.PI;
+			return area;
+		}
+
+		public override string ToString()
+		{
+			return $"Radius: {Radius}\nArea: {GetArea()}";
+		}
+
+	}
+
+}
